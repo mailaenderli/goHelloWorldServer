5ca49ecaab8b89196d3326dadf50e49723fe98af
Raphael Mailänder <61315599+mailaenderli@users.noreply.github.com>
2022-03-29 12:53:53 +0200

    try 5

--- hello_server.go
+++ hello_server.go
@@ -14,6 +14,10 @@ import (
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/trace"
+
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
+	"go.opentelemetry.io/otel/sdk/resource"
 )
 
 var tracer = otel.Tracer("github.com/mailaenderli/goHelloWorldServer")
@@ -54,7 +58,40 @@ func CreateGreeting(name string) string {
 	return "Hello, " + name + "\n"
 }
 
+func newResource() *resource.Resource {
+	return resource.NewWithAttributes(
+		semconv.SchemaURL,
+		semconv.ServiceNameKey.String("otlptrace-example"),
+		semconv.ServiceVersionKey.String("0.0.1"),
+	)
+}
+
+func installExportPipeline(ctx context.Context) func() {
+	client := otlptracehttp.NewClient()
+	exporter, err := otlptrace.New(ctx, client)
+	if err != nil {
+		log.Fatalf("creating OTLP trace exporter: %v", err)
+	}
+
+	tracerProvider := sdktrace.NewTracerProvider(
+		sdktrace.WithBatcher(exporter),
+		sdktrace.WithResource(newResource()),
+	)
+	otel.SetTracerProvider(tracerProvider)
+
+	return func() {
+		if err := tracerProvider.Shutdown(ctx); err != nil {
+			log.Fatalf("stopping tracer provider: %v", err)
+		}
+	}
+}
+
 func main() {
+	ctx := context.Background()
+	// Registers a tracer Provider globally.
+	cleanup := installExportPipeline(ctx)
+	defer cleanup()
+
 	// Create Server and Route Handlers
 	handler := http.HandlerFunc(httpHandler)
 	wrappedHandler := otelhttp.NewHandler(handler, "httpHandler-instrumented")
