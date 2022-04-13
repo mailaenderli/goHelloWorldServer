9fb84940f985351a5a4b0e219ab5835348c9c2fc
Raphael Mailänder <mailarap@students.zhaw.ch>
2022-04-13 17:47:01 +0200

    test

--- release.config.js
+++ release.config.js
@@ -2,9 +2,5 @@ module.exports = {
     branches: ['main'],
     plugins: [
         "@semantic-release/commit-analyzer",
-        ['@semantic-release/exec', {
-            publishCmd: ". ./setenvVar.sh ${nextRelease.version}"
-          }
-        ]
     ]
 }
